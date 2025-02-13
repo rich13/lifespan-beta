6d1d3413ebd8ac9167297c30e60849f3b51d7574
Richard Northover <richard@northover.info>
2025-02-13 17:06:25 +0000

    chore: Configure debugging and logging
    - Update logging configuration
    
    - Configure Ray for debugging
    
    - Set up Laravel Telescope

--- config/logging.php
+++ config/logging.php
@@ -54,7 +54,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily', 'slack'],
+            'channels' => ['daily'],
             'ignore_exceptions' => false,
         ],
 
