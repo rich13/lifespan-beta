54e336b75bb4f587cadbceca22f53973aa0b8c8b
Richard Northover <richard@northover.info>
2025-03-10 22:31:03 +0000

    Update JavaScript and home page views

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
 import 'bootstrap';
+import './dropdown-debug';
 
 import Alpine from 'alpinejs';
 
---
+++ resources/js/dropdown-debug.js
@@ -0,0 +1,4 @@
+/**
+ * This file is intentionally left empty.
+ * We're keeping it to avoid breaking imports, but we no longer need the debugging code.
+ */ 
