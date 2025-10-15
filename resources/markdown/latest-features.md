3e7c3ec48707bf47f261211ad299f72274173d58
Richard Northover <richard@northover.info>
2025-10-15 10:47:05 +0100

    Add About Lifespan modal with latest features and ontology documentation

---
+++ resources/markdown/latest-features.md
@@ -0,0 +1,5 @@
+- **This modal** to show what's new :-)
+- Some new ways to look at **photos** and the things featured in them
+- Improved **story generation** for more span types
+- Some new ways to **summarise** and **navigate** education and employment
+
---
+++ resources/views/components/modals/about-lifespan-modal.blade.php
@@ -0,0 +1,76 @@
+<!-- About Lifespan Modal -->
+<div class="modal fade" id="aboutLifespanModal" tabindex="-1" aria-labelledby="aboutLifespanModalLabel" aria-hidden="true" style="z-index: 1055;">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="aboutLifespanModalLabel">
+                    <i class="bi bi-bar-chart-steps me-2"></i>About Lifespan
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-4">
+                    <div class="alert alert-success">
+                    <h6 class="fw-semibold mb-2"><i class="bi bi-emoji-smile me-2"></i>Hello!</h6>
+                    <ul>
+                        <li>Lifespan is a platform for mapping and exploring time.</li>
+                        <li>This is a <strong>proto-prototype</strong>, built in a very random and exploratory way...</li>
+                    </ul>
+                    </div>
+                </div>
+                
+                <div class="mb-4">
+                    <div class="alert alert-primary">
+                    <h6 class="fw-semibold mb-2"><i class="bi bi-megaphone me-2"></i>Latest Features</h6>
+                        @php
+                            $latestFeaturesPath = resource_path('markdown/latest-features.md');
+                            $latestFeatures = file_exists($latestFeaturesPath) 
+                                ? Illuminate\Support\Str::markdown(file_get_contents($latestFeaturesPath))
+                                : '<ul><li>No features to display</li></ul>';
+                        @endphp
+                        {!! $latestFeatures !!}
+                    </div>
+                </div>
+                
+                <div class="mb-4">
+                    
+                    <div class="alert alert-danger">
+                        <div class="row">
+                            <div class="col-sm-4"><strong>Version:</strong></div>
+                            <div class="col-sm-8">{{ \App\Helpers\GitVersionHelper::getVersion() }}</div>
+                        </div>
+                        @php
+                            $versionDetails = \App\Helpers\GitVersionHelper::getDetailedVersion();
+                        @endphp
+                        @if($versionDetails)
+                            @if(isset($versionDetails['commit']))
+                                <div class="row mt-1">
+                                    <div class="col-sm-4"><strong>Commit:</strong></div>
+                                    <div class="col-sm-8"><code>{{ substr($versionDetails['commit'], 0, 8) }}</code></div>
+                                </div>
+                            @endif
+                            @if(isset($versionDetails['date']))
+                                <div class="row mt-1">
+                                    <div class="col-sm-4"><strong>Build Date:</strong></div>
+                                    <div class="col-sm-8">{{ $versionDetails['date'] }}</div>
+                                </div>
+                            @endif
+                        @endif
+                    </div>
+                </div>
+                
+                <div class="mb-4">
+                    <div class="alert alert-warning">
+                        <p class="mb-0">
+                            Coming soon: <a href="https://info.lifespan.dev" target="_blank" class="alert-link">https://info.lifespan.dev</a>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
